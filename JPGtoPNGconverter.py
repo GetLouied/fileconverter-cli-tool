3bf5073871010bc217ebed2ecafb705429f25e4f
Louis DeRienzo <derienzo_louis@yahoo.com>
2024-06-05 16:45:43 -0400

    initial commit

---
+++ JPGtoPNGconverter.py
@@ -0,0 +1,11 @@
+import sys
+import os
+from PIL import Image
+
+# grab first and second argument
+
+# Check is new / exists, if not create
+
+# loop through example folder
+# Convert images to png
+# save to the new folder
